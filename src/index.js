509334b970d14407c484de0d9301c7f5f57040c8
johnnyjychen <johnnyuchen@gmail.com>
2025-08-03 18:43:09 +0900

    Update.index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import './styles/App.css';
 import App from './App';
