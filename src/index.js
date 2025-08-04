85484b7d7f3bf16fbbfaf18de7d4147a85421e04
johnnyjychen <johnnyuchen@gmail.com>
2025-08-04 10:42:24 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/jychen">
+    <BrowserRouter basename="/jychen-website">
       <App />
     </BrowserRouter>
   </React.StrictMode>
