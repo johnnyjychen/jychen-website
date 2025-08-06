28067539561a880c62b92da594e0b7cd0893f762
johnnyjychen <johnnyuchen@gmail.com>
2025-08-06 21:33:48 +0800

    Update Publications.js

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -2,10 +2,12 @@ const Publications = () => {
     return (
         <div className="project-container">
             <h1>Publication</h1>
+            <h2>Peer-reviewd Conference Papers</h2>
             <ol>
-                <li>Cha, Y. J., <span className='highlight-green'>Chen, J.</span>, Gunal, Y., Zhu, Q., Newman, M. W., and Park, S. Y. 2025.
-                Collaborative Health-Tracking Technologies for Children and Parents: A Review of Current Studies and Directions for Future Research.
-                In Proceedings of the 2025 CHI Conference on Human Factors in Computing Systems (CHI 2025). [<a href='https://dl.acm.org/doi/full/10.1145/3706598.3713596' target="_blank">doi</a>] [<a href='https://dl.acm.org/doi/pdf/10.1145/3706598.3713596' target="_blank">pdf</a>]
+                <li>
+                    Cha, Y. J., <span className='highlight-green'>Chen, J.</span>, Gunal, Y., Zhu, Q., Newman, M. W., and Park, S. Y. 2025.
+                    Collaborative Health-Tracking Technologies for Children and Parents: A Review of Current Studies and Directions for Future Research.
+                    In Proceedings of the 2025 CHI Conference on Human Factors in Computing Systems (CHI 2025). [<a href='https://dl.acm.org/doi/full/10.1145/3706598.3713596' target="_blank">doi</a>] [<a href='https://dl.acm.org/doi/pdf/10.1145/3706598.3713596' target="_blank">pdf</a>]
                 </li>
             </ol>
         </div>
