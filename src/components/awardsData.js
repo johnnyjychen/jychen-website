455a6da5fa9ad702d988c2be55fb2347725f3ac1
johnnyjychen <johnnyuchen@gmail.com>
2025-12-16 13:42:56 -0600

    update styles

--- src/components/awardsData.js
+++ src/components/awardsData.js
@@ -1,6 +1,6 @@
 const awardsData = [
-  { date: "Feburary, 2025", content: "Received Fulton Fellowship from Arizona State University." },
-  { date: "March, 2023", content: "Received Merit-Based UMSI Half Tuition Scholarship from the University of Michigan ($50,000)." },
+  { date: "2025-02", content: "Received Fulton Fellowship from Arizona State University." },
+  { date: "2023-03", content: "Received Merit-Based UMSI Half Tuition Scholarship from the University of Michigan ($50,000)." },
 ];
 
 export default awardsData;
